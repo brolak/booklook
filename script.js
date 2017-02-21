7dcd8508c998d92ade5b53bb7eccc72c2766ee6f
Mati <matiborlak@gmail.com>
2017-02-21 15:19:43 +0200

    js checkout readd

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 $books = $(".books");
+$isbn = $("#book-name");
 
 var fetch = function (isbn) {
   $.ajax({
     method: "GET",
-    url: "https://www.googleapis.com/books/v1/volumes?q=isbn:0439023521",
+    url: "https://www.googleapis.com/books/v1/volumes?q=isbn:"+isbn,
     dataType: "json",
     success: function(data) {
       $books.empty();
-      console.log(data);
       $books.append("<h1>"+data.items[0].volumeInfo.title+"</h1>");
       $books.append("<p>"+data.items[0].volumeInfo.description+"</p>");
       $books.append("<h3>"+data.items[0].volumeInfo.authors[0]+"</h3>");
       $books.append("<img src="+data.items[0].volumeInfo.imageLinks.thumbnail+"/>");
+      $isbn.val('');
     },
     error: function(jqXHR, textStatus, errorThrown) {
       console.log(textStatus);
@@ -20,5 +21,5 @@ var fetch = function (isbn) {
 };
 
 $(".btn").on("click",function () {
-  fetch();
+  fetch($isbn.val());
 });
